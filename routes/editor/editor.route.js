406917154e945518930784117244de106384dec6
DuyHau <duyhau9898@gmail.com>
2019-06-10 01:43:23 +0700

    update editor

--- routes/editor/editor.route.js
+++ routes/editor/editor.route.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var hbscontent = require('../../app');
-
+//post
+var postModel = require('../../models/post.model');
 //=================================== Duyệt bài viết ===================================
 
 router.get('/', (req, res) => {
@@ -13,17 +14,78 @@ router.get('/', (req, res) => {
 
 router.get('/approvepost', (req, res) => {    
     
-    res.render('editor/editor-approvepost', hbscontent);
+    postModel.all()
+    .then(rows => {
+        console.log(rows);
+
+        hbscontent['posts'] = rows;
+        //Update totalpost in category table
+        rows.forEach(element => {
+            postModel.update(element).then().catch(err => { console.log(err)});
+            if(element.status=="Chưa duyệt")
+            {
+                element['isUnapproved'] = true; 
+            }
+            else if(element.status=="Đã duyệt")
+            {
+                element['isApproved'] = true; 
+            }
+            else if(element.status=="Từ chối")
+            {
+                element['isRejected'] = true; 
+            }
+        });
+       
+        console.log(rows);
+        res.render('editor/editor-approvepost', hbscontent);
+    }).catch(err => {
+        console.log(err);
+    }); 
 });
 
-router.get('/approvedpost', (req, res) => {
+
+router.post('/approvedpost/:id', (req, res) => {
     
-    res.render('editor/editor-approvedpost', hbscontent);
+    var entity = req.body;
+    console.log(entity);
+    var id = req.params.id;
+    postModel.single(id)
+    .then(rows=>{
+        rows[0].status = "Đã duyệt";
+        postModel.update(rows[0]).then(()=>{
+            res.redirect('/editor/approvepost');
+        }) 
+        .catch(err => {
+            console.log(err);
+            res.end('Error occured');
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.end('Error occured');
+    });
+    
 });
 
-router.get('/rejectedpost', (req, res) => {
+router.post('/rejectedpost/:id', (req, res) => {
 
-    res.render('editor/editor-rejectedpost', hbscontent);
+    
+    var id = req.params.id;
+    postModel.single(id)
+    .then(rows=>{
+        rows[0].status = "Từ chối";
+        postModel.update(rows[0]).then(()=>{
+            res.redirect('/editor/approvepost');
+        }) 
+        .catch(err => {
+            console.log(err);
+            res.end('Error occured');
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.end('Error occured');
+    });
 });
 
 router.get('/editprofile', (req, res) => {
